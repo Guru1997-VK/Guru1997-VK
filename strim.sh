6fa14e5e2d2aaf3f5751a9a6dad8f2e2740932a3
Vivek Kumar <96232735+gurugitcode@users.noreply.github.com>
2022-02-16 20:02:39 +0530

    strem.sh

---
+++ strim.sh
@@ -0,0 +1,10 @@
+sudo apt-get update -y
+sudo apt-get install gstreamer1.0-tools gstreamer1.0-alsa -y
+sudo apt-get install gstreamer1.0-plugins-base gstreamer1.0-plugins-good -y
+sudo apt-get install gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly -y
+sudo apt-get install gstreamer1.0-libav -y
+sudo apt-get install libgstreamer1.0-dev -y
+sudo apt-get install libgstreamer-plugins-base1.0-dev -y
+sudo apt-get install gstreamer1.0-plugins-bad -y
+
+
